1bd57d2aa3751cdf9089aade9cfee818792f11ce
HuangKun <diueym@gmail.com>
2016-06-04 23:08:43 +0800

    add new indicator appearance(line and points

--- Example/OMChartView/ExampleInteractiveView.swift
+++ Example/OMChartView/ExampleInteractiveView.swift
@@ -11,25 +11,39 @@ import OMChartView
 
 class ExampleInteractiveView: OMChartInteractiveView {
     var indicatorLine: CALayer?
+    var indicatorPoints: [CALayer] = []
+    
+    private lazy var indicatorImage: UIImage? = {
+        return UIImage(named: "statistic_indicator_point")
+    }()
     
     override func panBegan(location: CGPoint, _ currentOffsets: [CGPoint]) {
-//        indicatorLine = currentIndicatroLineLayer(currentOffsets)
-//        self.layer.addSublayer(indicatorLine!)
+        currentIndicatorPointLayers(currentOffsets)
     }
     
     override func panChanged(location: CGPoint, _ currentOffsets: [CGPoint]) {
         indicatorLine?.removeFromSuperlayer()
         indicatorLine = nil
         
-        
         indicatorLine = currentIndicatroLineLayer(currentOffsets)
-            print(currentOffsets)
         self.layer.addSublayer(indicatorLine!)
+        
+        for point in indicatorPoints {
+            point.removeFromSuperlayer()
+        }
+        indicatorPoints.removeAll()
+        currentIndicatorPointLayers(currentOffsets)
+        
     }
     
     override func panEnded(location: CGPoint, _ currentOffsets: [CGPoint]) {
         indicatorLine?.removeFromSuperlayer()
         indicatorLine = nil
+        
+        for point in indicatorPoints {
+            point.removeFromSuperlayer()
+        }
+        indicatorPoints.removeAll()
     }
     
     
@@ -47,9 +61,40 @@ class ExampleInteractiveView: OMChartInteractiveView {
         
         let layer = CAShapeLayer()
         layer.path = path
-        layer.strokeColor = UIColor.yellowColor().CGColor
+        layer.strokeColor = UIColor(white: 1, alpha: 0.58).CGColor
         layer.lineWidth = 2
         
         return layer
     }
+    
+    private func currentIndicatorPointLayers(withCurrentOffsets: [CGPoint]) {
+//        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { 
+//            UIGraphicsBeginImageContext(CGSize(width: 20, height: 20))
+//            UIImage(named: "statistic_indicator_point")?.drawInRect(CGRect(origin: CGPointZero, size: CGSize(width: 20, height: 20)))
+//            let image = UIGraphicsGetImageFromCurrentImageContext()
+//            
+//            dispatch_async(dispatch_get_main_queue(), { 
+//                for item in withCurrentOffsets {
+//                    let layer = CALayer()
+//                    layer.frame = CGRect(origin: CGPointZero, size: image.size)
+//                    layer.position = item
+//                    layer.contents = image.CGImage
+//                    self.indicatorPoints.append(layer)
+//                    self.layer.addSublayer(layer)
+//                }
+//            })
+//        }
+        
+        guard let image = indicatorImage else { return }
+        
+        for item in withCurrentOffsets {
+            let layer = CALayer()
+            layer.frame = CGRect(origin: CGPointZero, size: image.size)
+            layer.position = item
+            layer.contents = image.CGImage
+            self.indicatorPoints.append(layer)
+            self.layer.addSublayer(layer)
+        }
+    }
+    
 }
